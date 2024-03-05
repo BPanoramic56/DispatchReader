3d60fd40910f368cb73eeb290b875342f7771bb9
brunogomespascotto <panoramic56@proton.me>
2024-03-05 08:38:03 -0700

    Initial Commit

---
+++ Reader/Program.cs
@@ -0,0 +1,10 @@
+namespace Reader;
+public class Reader{
+    public Reader(){
+
+    }
+
+    // static void Main(string[] args){
+    //     Console.WriteLine("a");
+    // }
+}
---
+++ Reader/Tester.cs
@@ -0,0 +1,11 @@
+
+namespace Reader
+{
+	public class EvaluatorTester
+	{
+		public static void Main(string[] args)
+		{
+            Console.WriteLine("wwww909");
+        }
+    }
+}
